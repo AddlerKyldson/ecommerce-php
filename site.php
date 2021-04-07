e9f4f823832f70f83e67c497f511b50542911068
Addler Kyldson <adler.inacio@gmail.com>
2021-04-07 18:00:08 -0300

    WEB Service Endereço

--- site.php
+++ site.php
@@ -144,18 +144,103 @@ $app->post("/cart/freight", function(){
 
 $app->get("/checkout", function(){
 
-	User::verifyLogin(false);    
+	User::verifyLogin(false);   
+	
+	$address = new Address();
 
 	$cart = Cart::getFromSession();
 
-	$address = new Address();
+	if(isset($_GET['zipcode'])){
+
+		$_GET['zipcode'] = $cart->getdeszipcode();
+
+	}
+
+	if(isset($_GET['zipcode'])){
+		
+		$address->loadFromCEP($_GET['zipcode']);
+
+		$cart->setdeszipcode($_GET['zipcode']);
+
+		$cart->save();
+
+		$cart->getCalculateTotal();
+
+	}
+
+	if(!$address->getdesaddress()) $address->setdesaddress('');
+	if(!$address->getdescomplement()) $address->setdescomplement('');
+	if(!$address->getdesdistrict()) $address->setdesdistrict('');
+	if(!$address->getdescity()) $address->setdescity('');
+	if(!$address->getdesstate()) $address->setdesstate('');
+	if(!$address->getdescountry()) $address->setdescountry('');
+	if(!$address->getdeszipcode()) $address->setdeszipcode('');
+
 
 	$page = new Page();
 
 	$page->setTpl("checkout", [
 		'cart'=>$cart->getValues(),
-		'address'=>$address->getValues()
+		'address'=>$address->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>Address::getMsgError()
 	]);
+
+});
+
+$app->post("/checkout", function(){
+
+	User::verifyLogin(false); 
+
+	if(!isset($_POST['zipcode']) || $_POST['zipcode'] === ''){
+		Address::setMsgError("Preencha o CEP.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if(!isset($_POST['desaddress']) || $_POST['desaddress'] === ''){
+		Address::setMsgError("Preencha o Endereço.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if(!isset($_POST['desdistrict']) || $_POST['desdistrict'] === ''){
+		Address::setMsgError("Preencha o Bairro.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if(!isset($_POST['descity']) || $_POST['descity'] === ''){
+		Address::setMsgError("Preencha a Cidade.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if(!isset($_POST['desstate']) || $_POST['desstate'] === ''){
+		Address::setMsgError("Preencha o Estado.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	if(!isset($_POST['descountry']) || $_POST['descountry'] === ''){
+		Address::setMsgError("Preencha o País.");
+		header('Location: /checkout');
+		exit;
+	}
+
+	$user = User::getFromSession();
+
+	$address = new Address();
+
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	$_POST['idperson'] = $user->getidperson();
+
+	$address->setData($_POST);
+
+	$address->save();
+
+	header('Location: /order');
+	exit;
 
 });
 
